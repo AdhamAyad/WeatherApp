ae22ada8feebb8a7d66aa78c4f4cfe807fafd36d
Adham Ayad <aboayad557@gmail.com>
2023-10-04 20:55:02 +0200

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'pacifico'
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: Icon(Icons.cloud , size: 32,),
+            )
+          ],
+          leading: const Icon(Icons.sunny , size: 32,),
+          backgroundColor: const Color(0XFF2D3250),
+          centerTitle: true,
+          title: const Text('Weather App' ,style: TextStyle(
+            fontSize: 24
+          ),),
+
+        ),
+      ),
+    );
+  }
+}
